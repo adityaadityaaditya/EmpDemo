543302d60049d4bc2bab2af59c3dd4fb8767370a
adityaadityaaditya <48765568+adityaadityaaditya@users.noreply.github.com>
2019-09-08 13:29:08 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,7 +8,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'construct.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+    except ImportError as exc;
+    #print('except')
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
